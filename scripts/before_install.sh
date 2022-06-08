81d532800b8aece76777059a2d2aa01cd571e505
Victor <v.huang435@gmail.com>
2022-06-08 12:14:16 +1000

    updated

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,8 +3,8 @@
 #download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install node
-
+nvm install --lts
+npm install -g pm2
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/express-app"
 if [ -d "$DIR" ]; then
